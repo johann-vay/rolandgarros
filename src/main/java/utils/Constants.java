36536220b61a2d9b5a8e3e4a94b4dfb7337e2042
Johann VAY <johann.vay@gmail.com>
2017-05-17 13:34:29 +0200

    Commit Modif pom.xml -> ibatis Add constants.java

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author Johann
+ */
+public class Constants {
+    
+    
+    public static String SQL_PATH_FILE = "rolandgarros.sql";
+    // Url de connexion en base de donnée
+    public static String DATABASE_URL = "jdbc:mysql://localhost:3306/rolandgarros";
+    // Utilisateur de la base de données
+    public static String DATABASE_USER = "root";
+    // Mot de passe de la base de données
+    public static String DATABASE_PASSWORD = "root";
+
+    // Drivers Jdbc
+    public static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+}
